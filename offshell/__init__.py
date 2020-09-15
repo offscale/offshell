95265839645e40f8559f33b659a0e549bcb30afe
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2020-09-15 15:11:03 +1000

    [README.md] New badges; [LICENSE-{APACHE,MIT}] Update copyright year range; [*.py] Python 3 support; Blacken; [requirements.txt,setup.py] Fix dependencies to reflect new names / Python 3

--- offshell/__init__.py
+++ offshell/__init__.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-from __future__ import print_function
 
 import logging
 from json import loads
@@ -17,12 +16,12 @@ from paramiko import SSHClient
 
 from offshell.interactive import interactive_shell
 
-__author__ = 'Samuel Marks'
-__version__ = '0.0.4'
+__author__ = "Samuel Marks"
+__version__ = "0.0.4"
 
 
 def get_logger(name=None):
-    with open(path.join(path.dirname(__file__), '_data', 'logging.yml'), 'rt') as f:
+    with open(path.join(path.dirname(__file__), "_data", "logging.yml"), "rt") as f:
         data = yaml.load(f)
     _dictConfig(data)
     return logging.getLogger(name=name)
@@ -30,53 +29,80 @@ def get_logger(name=None):
 
 root_logger = get_logger()
 
-logging.getLogger('paramiko').setLevel(logging.CRITICAL)
+logging.getLogger("paramiko").setLevel(logging.CRITICAL)
 
 
 def offshell(name, load_system_host_keys, ssh_config, etcd):
-    host, port = etcd.split(':')
+    host, port = etcd.split(":")
     node = dict_to_node(loads(Client(host=host, port=int(port)).get(name).value))
     if node.state != NodeState.RUNNING:
-        raise EnvironmentError('Node isn\'t running, it\'s {}. Ensure it\'s ON, then try again.'.format(node.state))
+        raise EnvironmentError(
+            "Node isn't running, it's {}. Ensure it's ON, then try again.".format(
+                node.state
+            )
+        )
 
-    connection_d = {'hostname': node.public_ips[0],
-                    'username': node.extra['user'] if 'user' in node.extra
-                    else node.extra.get('ssh_config', {}).get('User'),
-                    'password': node.extra.get('password'),
-                    'key_filename': node.extra.get('ssh_config', {}).get('IdentityFile')}
+    connection_d = {
+        "hostname": node.public_ips[0],
+        "username": node.extra["user"]
+        if "user" in node.extra
+        else node.extra.get("ssh_config", {}).get("User"),
+        "password": node.extra.get("password"),
+        "key_filename": node.extra.get("ssh_config", {}).get("IdentityFile"),
+    }
     if ssh_config:
-        if not connection_d['key_filename']:
-            root_logger.warn('Cannot set password in ssh_config format. You\'ll still be prompted.')
-        tab = ' ' * 4
+        if not connection_d["key_filename"]:
+            root_logger.warn(
+                "Cannot set password in ssh_config format. You'll still be prompted."
+            )
+        tab = " " * 4
 
-        if 'ssh_config' in node.extra and len(node.extra['ssh_config'].keys()) > 1:
-            host = node.extra['ssh_config'].pop('Host')
-            print('Host {host}\n{rest}'
-                  .format(host=host,
-                          rest=tab + tab.join('{} {}\n'.format(k, v[0] if isinstance(v, list) else v)
-                                              for k, v in node.extra['ssh_config'].iteritems())[:-1]))
+        if (
+            "ssh_config" in node.extra
+            and len(list(node.extra["ssh_config"].keys())) > 1
+        ):
+            host = node.extra["ssh_config"].pop("Host")
+            print(
+                "Host {host}\n{rest}".format(
+                    host=host,
+                    rest=tab
+                    + tab.join(
+                        "{} {}\n".format(k, v[0] if isinstance(v, list) else v)
+                        for k, v in node.extra["ssh_config"].items()
+                    )[:-1],
+                )
+            )
 
         else:
-            print('Host {name}\n'
-                  '{tab}HostName {hostname}\n'
-                  '{tab}User {username}\n'
-                  '{last_line}'
-                  .format(name=name.rpartition('/')[2], hostname=connection_d['hostname'],
-                          username=connection_d['username'], tab=tab,
-                          last_line='{tab}IdentityFile {key_filename}'.format(tab=tab,
-                                                                              key_filename=connection_d[
-                                                                                  'key_filename'])
-                          if connection_d['key_filename'] else ''))
-        known_hosts = path.join(expanduser('~'), '.ssh', 'known_hosts')
-        s = ''
+            print(
+                "Host {name}\n"
+                "{tab}HostName {hostname}\n"
+                "{tab}User {username}\n"
+                "{last_line}".format(
+                    name=name.rpartition("/")[2],
+                    hostname=connection_d["hostname"],
+                    username=connection_d["username"],
+                    tab=tab,
+                    last_line="{tab}IdentityFile {key_filename}".format(
+                        tab=tab, key_filename=connection_d["key_filename"]
+                    )
+                    if connection_d["key_filename"]
+                    else "",
+                )
+            )
+        known_hosts = path.join(expanduser("~"), ".ssh", "known_hosts")
+        s = ""
         if exists(known_hosts):
-            with open(known_hosts, 'rt') as f:
+            with open(known_hosts, "rt") as f:
                 s = f.read()
-        if connection_d['hostname'] not in s:
-            print('After checking the key fingerprint, add it to your known hosts with:'
-                  '\nssh-keyscan {host} >> {known_hosts}'.format(host=connection_d['hostname'],
-                                                                 known_hosts=known_hosts),
-                  file=stderr)
+        if connection_d["hostname"] not in s:
+            print(
+                "After checking the key fingerprint, add it to your known hosts with:"
+                "\nssh-keyscan {host} >> {known_hosts}".format(
+                    host=connection_d["hostname"], known_hosts=known_hosts
+                ),
+                file=stderr,
+            )
         return
 
     client = SSHClient()
--- offshell/__main__.py
+++ offshell/__main__.py
@@ -5,25 +5,50 @@ from offshell import __version__, offshell
 
 
 def _build_parser():
-    parser = ArgumentParser(description='offshell opens an interactive shell into your offscale node.')
-    parser.add_argument('-n', '--name',
-                        help='Name of node. /{purpose}/{node_name} will overwrite `--purpose`.', required=True)
-    parser.add_argument('--version', action='version',
-                        version='{} {}'.format(modules[__name__].__package__, __version__))
-    parser.add_argument('--etcd', help='Server location\t[127.0.0.1:4001]', default='127.0.0.1:4001')
-    parser.add_argument('--purpose', '--cluster', dest='purpose', default='unclustered',
-                        help='Purpose of the node. Groups all together (hierarchically). Defaults to: \'unclustered\'')
-    parser.add_argument('--load-system-host-keys', dest='load_system_host_keys', default=True,
-                        help='Load host keys from a system (read-only) file.')
-    parser.add_argument('-o', '--ssh-config', dest='ssh_config', action='store_true',
-                        help='Output SSH config format. Don\'t connect.')
+    parser = ArgumentParser(
+        description="offshell opens an interactive shell into your offscale node."
+    )
+    parser.add_argument(
+        "-n",
+        "--name",
+        help="Name of node. /{purpose}/{node_name} will overwrite `--purpose`.",
+        required=True,
+    )
+    parser.add_argument(
+        "--version",
+        action="version",
+        version="{} {}".format(modules[__name__].__package__, __version__),
+    )
+    parser.add_argument(
+        "--etcd", help="Server location\t[127.0.0.1:4001]", default="127.0.0.1:4001"
+    )
+    parser.add_argument(
+        "--purpose",
+        "--cluster",
+        dest="purpose",
+        default="unclustered",
+        help="Purpose of the node. Groups all together (hierarchically). Defaults to: 'unclustered'",
+    )
+    parser.add_argument(
+        "--load-system-host-keys",
+        dest="load_system_host_keys",
+        default=True,
+        help="Load host keys from a system (read-only) file.",
+    )
+    parser.add_argument(
+        "-o",
+        "--ssh-config",
+        dest="ssh_config",
+        action="store_true",
+        help="Output SSH config format. Don't connect.",
+    )
     return parser
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = _build_parser().parse_args()
-    if args.purpose and not args.name.startswith('/'):
-        args.name = '/'.join((args.purpose, args.name))
+    if args.purpose and not args.name.startswith("/"):
+        args.name = "/".join((args.purpose, args.name))
         args.purpose = None
 
     offshell(**dict((k, v) for k, v in args._get_kwargs() if v is not None))
